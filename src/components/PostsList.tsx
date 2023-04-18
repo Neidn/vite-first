9953c5cc53f6726674ac612935e94a507e643fee
Neidn <dty3152@gmail.com>
2023-04-18 15:53:21 +0900

    add exception with no posts

--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -26,11 +26,20 @@ const PostsList = ({isPosting, onStopPosting}: PostsListModel) => {
                   onCancel={onStopPosting}/>
             </Modal>
         }
-        <ul className={classes.posts}>
-          {posts.map((post, index) => (
-              <Post key={index} author={post.author} body={post.body}/>
-          ))}
-        </ul>
+        {posts.length > 0 &&
+            <ul className={classes.posts}>
+              {posts.map((post, index) => (
+                  <Post key={index} author={post.author} body={post.body}/>
+              ))}
+            </ul>
+        }
+        {posts.length === 0 &&
+            <div className={classes.noPosts}>
+              <h2>There are no posts yet.</h2>
+              <p>Start adding some!</p>
+            </div>
+
+        }
       </>
   )
 }
