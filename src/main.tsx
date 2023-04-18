2cb19fdfd1d0cbb0c8a04d0c08149727b20ff47f
Neidn <dty3152@gmail.com>
2023-04-18 16:40:34 +0900

    start to use routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import {RouterProvider, createBrowserRouter} from "react-router-dom";
+
 import './index.css'
+
+import App from './App'
+
+const router = createBrowserRouter([
+  {path: '/', element: <App/>},
+  {path: '/:id', element: <App/>}
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-      <App/>
-    </React.StrictMode>,
-)
+      <RouterProvider router={router}/>
+    </React.StrictMode>
+);
