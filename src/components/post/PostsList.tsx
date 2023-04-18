82466e749587f4adaee5b60da4c9abe83c1672eb
Neidn <dty3152@gmail.com>
2023-04-18 16:31:26 +0900

    control data requesting

--- src/components/post/PostsList.tsx
+++ src/components/post/PostsList.tsx
@@ -1,4 +1,4 @@
-import {useState, useEffect} from "react";
+import {useState, useEffect, useReducer} from "react";
 import axios from "axios";
 
 import classes from './PostsList.module.css'
@@ -9,20 +9,47 @@ import Post from "./Post";
 import NewPost from "./NewPost";
 import Modal from "../common/Modal";
 
+const httpInitState = {
+  isFetching: false,
+  error: '',
+}
+
+const httpReducer = (state: any, action: any) => {
+  switch (action.type) {
+    case 'SEND':
+      return {isFetching: true, error: ''};
+    case 'RESPONSE':
+      return {...state, isFetching: false};
+    case 'ERROR':
+      return {isFetching: false, error: action.errorMessage};
+    case 'CLEAR':
+      return httpInitState;
+    default:
+      throw new Error('Should not be reached!');
+  }
+}
+
 const URL = `${import.meta.env.VITE_DUMMY_URL}/${import.meta.env.VITE_DUMMY_DB}`;
 
 const PostsList = ({isPosting, onStopPosting}: PostsListModel) => {
   const [posts, setPosts] = useState<PostModel[]>([]);
+  const [httpState, dispatchHttp] = useReducer(httpReducer, httpInitState);
 
   useEffect(() => {
-    const axiosPosts = async () => {
-      const response = await axios.get(URL);
+    try {
+      const axiosPosts = async () => {
+        dispatchHttp({type: 'SEND'});
+        const response = await axios.get(URL);
 
-      setPosts(response.data.posts);
+        setPosts(response.data.posts);
+        dispatchHttp({type: 'RESPONSE'});
+      }
+      axiosPosts().catch((error) => {
+        throw new Error(error);
+      });
+    } catch (error) {
+      dispatchHttp({type: 'ERROR', errorMessage: 'Something went wrong!'});
     }
-    axiosPosts().catch((error) => {
-      console.log(error);
-    });
   }, []);
 
   const addPostHandler = async (post: PostModel) => {
@@ -42,19 +69,23 @@ const PostsList = ({isPosting, onStopPosting}: PostsListModel) => {
                   onCancel={onStopPosting}/>
             </Modal>
         }
-        {posts.length > 0 &&
+        {!httpState.isFetching && posts.length > 0 &&
             <ul className={classes.posts}>
               {posts.map((post, index) => (
                   <Post key={index} author={post.author} body={post.body}/>
               ))}
             </ul>
         }
-        {posts.length === 0 &&
+        {!httpState.isFetching && posts.length === 0 &&
             <div className={classes.noPosts}>
               <h2>There are no posts yet.</h2>
               <p>Start adding some!</p>
             </div>
-
+        }
+        {httpState.isFetching &&
+            <div className={classes.loading}>
+              <p>Loading...</p>
+            </div>
         }
       </>
   )
